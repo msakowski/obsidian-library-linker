1e26034440abc48909e9f485f5fd0694ea634103
Martin Sakowski <martin.sakowski@gmail.com>
2025-02-19 14:24:36 +0100

    Fix tests to work with short/long format

--- src/__tests__/bible-reference.test.ts
+++ src/__tests__/bible-reference.test.ts
@@ -45,33 +45,55 @@ describe('LibraryLinkerPlugin', () => {
   describe('convertBibleTextToMarkdownLink', () => {
     const testCases = [
       {
-        input: 'offb21:3',
-        expected: '[Offenbarung 21:3](jwlibrary:///finder?bible=66021003)',
+        input: 'off21:3',
+        expectedLong: '[Offenbarung 21:3](jwlibrary:///finder?bible=66021003)',
+        expectedShort: '[Off 21:3](jwlibrary:///finder?bible=66021003)',
       },
       {
         input: '1mo1:1',
-        expected: '[1. Mose 1:1](jwlibrary:///finder?bible=01001001)',
+        expectedLong: '[1. Mose 1:1](jwlibrary:///finder?bible=01001001)',
+        expectedShort: '[1Mo 1:1](jwlibrary:///finder?bible=01001001)',
       },
       {
         input: 'ps 23:1-3',
-        expected: '[Psalm 23:1-3](jwlibrary:///finder?bible=19023001-19023003)',
+        expectedLong: '[Psalm 23:1-3](jwlibrary:///finder?bible=19023001-19023003)',
+        expectedShort: '[Ps 23:1-3](jwlibrary:///finder?bible=19023001-19023003)',
       },
       {
         input: 'hoh1:1',
-        expected: '[Hohes Lied 1:1](jwlibrary:///finder?bible=22001001)',
+        expectedLong: '[Hohes Lied 1:1](jwlibrary:///finder?bible=22001001)',
+        expectedShort: '[Hoh 1:1](jwlibrary:///finder?bible=22001001)',
       },
       {
         input: 'ps105:3',
-        expected: '[Psalm 105:3](jwlibrary:///finder?bible=19105003)',
+        expectedLong: '[Psalm 105:3](jwlibrary:///finder?bible=19105003)',
+        expectedShort: '[Ps 105:3](jwlibrary:///finder?bible=19105003)',
       },
       {
         input: 'Ps 29:10',
-        expected: '[Psalm 29:10](jwlibrary:///finder?bible=19029010)',
+        expectedLong: '[Psalm 29:10](jwlibrary:///finder?bible=19029010)',
+        expectedShort: '[Ps 29:10](jwlibrary:///finder?bible=19029010)',
       },
     ];
 
-    test.each(testCases)('formats "$input" to "$expected"', ({ input, expected }) => {
-      expect(plugin.convertBibleTextToMarkdownLink(input)).toBe(expected);
+    describe('with long format', () => {
+      beforeEach(() => {
+        plugin.settings = { ...plugin.settings, useShortNames: false };
+      });
+
+      test.each(testCases)('formats "$input" to "$expectedLong"', ({ input, expectedLong }) => {
+        expect(plugin.convertBibleTextToMarkdownLink(input)).toBe(expectedLong);
+      });
+    });
+
+    describe('with short format', () => {
+      beforeEach(() => {
+        plugin.settings = { ...plugin.settings, useShortNames: true };
+      });
+
+      test.each(testCases)('formats "$input" to "$expectedShort"', ({ input, expectedShort }) => {
+        expect(plugin.convertBibleTextToMarkdownLink(input)).toBe(expectedShort);
+      });
     });
   });
 
@@ -123,24 +145,44 @@ describe('LibraryLinkerPlugin', () => {
     const testCases = [
       {
         input: 'offb21:3',
-        expected: 'Offenbarung 21:3',
+        expectedLong: 'Offenbarung 21:3',
+        expectedShort: 'Off 21:3',
       },
       {
         input: '1mo1:1',
-        expected: '1. Mose 1:1',
+        expectedLong: '1. Mose 1:1',
+        expectedShort: '1Mo 1:1',
       },
       {
         input: 'hoh 1:1',
-        expected: 'Hohes Lied 1:1',
+        expectedLong: 'Hohes Lied 1:1',
+        expectedShort: 'Hoh 1:1',
       },
       {
         input: 'ps23:1-3',
-        expected: 'Psalm 23:1-3',
+        expectedLong: 'Psalm 23:1-3',
+        expectedShort: 'Ps 23:1-3',
       },
     ];
 
-    test.each(testCases)('formats "$input" to "$expected"', ({ input, expected }) => {
-      expect(plugin['formatBibleText'](input)).toBe(expected);
+    describe('with long format', () => {
+      beforeEach(() => {
+        plugin.settings = { ...plugin.settings, useShortNames: false };
+      });
+
+      test.each(testCases)('formats "$input" to "$expectedLong"', ({ input, expectedLong }) => {
+        expect(plugin['formatBibleText'](input)).toBe(expectedLong);
+      });
+    });
+
+    describe('with short format', () => {
+      beforeEach(() => {
+        plugin.settings = { ...plugin.settings, useShortNames: true };
+      });
+
+      test.each(testCases)('formats "$input" to "$expectedShort"', ({ input, expectedShort }) => {
+        expect(plugin['formatBibleText'](input)).toBe(expectedShort);
+      });
     });
   });
 });
