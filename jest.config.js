087f08c220503e06597a32a00a7e6c8898d9e40a
Nils Schönwald <nils@schoen.world>
2025-04-04 19:14:44 +0200

    fix config

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,10 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   transform: {
-    '^.+\\.tsx?$': '@swc/jest',
+    '^.+\\.[jt]sx?$': '@swc/jest',
   },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-  moduleFileExtensions: ['ts', 'tsx', 'json', 'node'],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
     '^mocks/(.*)$': '<rootDir>/__mocks__/$1',
